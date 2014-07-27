2736dc9764cf475306993c4f34ecbf9932d5a140
Yan <xiaoyanc210@gmail.com>
2014-07-27 19:36:52 -0400

    initial

---
+++ MovieTogether/PageContentViewController.h
@@ -0,0 +1,18 @@
+//
+//  PageContentViewController.h
+//  MovieTogether
+//
+//  Created by Xiaoyan Cai on 7/27/14.
+//  Copyright (c) 2014 yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PageContentViewController : UIViewController
+
+@end
+
+
+@property NSString *movieName;
+@property NSString *imageFile;
+
