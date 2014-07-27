dd27e574e3367a3765f63be36726d5dcfb5457fe
Yan <xiaoyanc210@gmail.com>
2014-07-27 14:32:54 -0400

    Initial Commit

---
+++ MovieTogether/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MovieTogether
+//
+//  Created by Xiaoyan Cai on 7/27/14.
+//  Copyright (c) 2014 yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MovieTogether/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  MovieTogether
+//
+//  Created by Xiaoyan Cai on 7/27/14.
+//  Copyright (c) 2014 yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
