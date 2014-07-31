960a4a697fba8ee0c821459ecc5d3b7a3b49c40a
Yan <xiaoyanc210@gmail.com>
2014-07-31 12:51:41 -0400

    change file destination

---
+++ MovieTogether/User.h
@@ -0,0 +1,13 @@
+//
+//  User.h
+//  MovieTogether
+//
+//  Created by Xiaoyan Cai on 7/31/14.
+//  Copyright (c) 2014 yan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@end
---
+++ MovieTogether/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  MovieTogether
+//
+//  Created by Xiaoyan Cai on 7/31/14.
+//  Copyright (c) 2014 yan. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
